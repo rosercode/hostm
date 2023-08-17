ad006c108222ab4f7b46307f5cbd6b905815277f
rosercode <rosercode@gmail.com>
2023-08-17 18:48:20 +0800

    feat init

---
+++ src/main/java/cn/com/rosercode/hostm/HostmApplication.java
@@ -0,0 +1,13 @@
+package cn.com.rosercode.hostm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostmApplication.class, args);
+    }
+
+}
