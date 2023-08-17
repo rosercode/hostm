ad71340da8591cd8b0e3a17392f5fb6e8a8c5fea
rosercode <rosercode@gmail.com>
2023-08-17 18:54:47 +0800

    feat/fix modify method that check whether target host is reachable

--- src/main/java/cn/com/rosercode/hostm/utils/HostReachabilityChecker.java
+++ src/main/java/cn/com/rosercode/hostm/utils/HostReachabilityChecker.java
@@ -1,5 +1,6 @@
 package cn.com.rosercode.hostm.utils;
 
+import java.io.IOException;
 import java.net.InetAddress;
 
 
@@ -11,10 +12,15 @@ import java.net.InetAddress;
 public class HostReachabilityChecker {
     /**
      * 判断 ip address 是否可达
+     *
      * @param ipAddress
      * @return
      */
     public static boolean isReachable(String ipAddress) {
+        // 先调用系统命令来判断主机是否可达
+        if (ping(ipAddress)){
+            return true;
+        }
         int timeoutMillis = 5000;
         try {
             InetAddress inetAddress = InetAddress.getByName(ipAddress);
@@ -24,4 +30,27 @@ public class HostReachabilityChecker {
         }
     }
 
+    public static boolean ping(String ipAddress) {
+
+        String os = System.getProperty("os.name").toLowerCase();
+        ProcessBuilder processBuilder;
+        if (os.contains("win")) {
+            // Windows
+            processBuilder = new ProcessBuilder("ping", "-n", "4", ipAddress);
+        } else {
+            // Linux and others
+            processBuilder = new ProcessBuilder("ping", "-c", "4", ipAddress);
+        }
+        try {
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (IOException | InterruptedException e) {
+            return false;
+        }
+    }
 }
